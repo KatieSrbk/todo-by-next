738c5e0b94b8fe09a6649230ae2817b4ea5d53ba
Екатерина <miss.kate.sh@gmail.com>
2026-04-20 15:38:02 +0300

    feat: добавить базовый функционал TodoList - отображение задач

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,15 +1,54 @@
+'use client';
+
+import { useState } from 'react';
 import AddSection from '../AddSection';
 import FilterGroup from '../FilterGroup';
 import TodosItems from '../TodosItems';
 import styles from './styles.module.scss';
 
+const TODOS_MOCK = [
+  { id: 1, text: 'Пример задачи 1', completed: false, createdAt: Date.now() },
+  {
+    id: 2,
+    text: 'Пример задачи 2',
+    completed: false,
+    createdAt: Date.now() + 1000,
+  },
+  {
+    id: 3,
+    text: 'Пример задачи 3',
+    completed: true,
+    createdAt: Date.now() + 2000,
+  },
+  {
+    id: 4,
+    text: 'Пример задачи 4',
+    completed: false,
+    createdAt: Date.now() + 3000,
+  },
+];
+
 const TodoList = () => {
+  const [todos, setTodos] = useState(TODOS_MOCK);
+
+  const addTodo = (text) => {
+    if (text.trim()) {
+      const newTodo = {
+        id: Date.now(),
+        text: text.trim(),
+        completed: false,
+        createdAt: Date.now(),
+      };
+      setTodos([...todos, newTodo]);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.title}>Список дел</h1>
-      <AddSection />
+      <AddSection addTodo={addTodo} />
       <FilterGroup />
-      <TodosItems />
+      <TodosItems todos={todos} />
     </div>
   );
 };
--- src/components/TodosItems/index.jsx
+++ src/components/TodosItems/index.jsx
@@ -1,21 +1,14 @@
 import styles from './styles.module.scss';
 
-const arrayOfTodos = [
-  'Пример задачи 1',
-  'Пример задачи 2',
-  'Пример задачи 3',
-  'Пример задачи 4',
-];
-
-const TodosItems = () => {
+const TodosItems = ({ todos }) => {
   return (
     <div className={styles.todoList}>
-      {arrayOfTodos.map((item, index) => {
+      {todos.map((item, index) => {
         return (
           <div key={index} className={styles.todoItem}>
             <div className={styles.todoWrapper}>
               <input className={styles.todoCheckbox} type='checkbox' />
-              <span className={styles.todoText}>{item}</span>
+              <span className={styles.todoText}>{item.text}</span>
             </div>
             <button className={styles.deleteButton}>Удалить</button>
           </div>
